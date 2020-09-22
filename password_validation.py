5151f266f40cd2f8523ce1072e7567c253e0ce8a
Racine McLean <racine.mclean87@gmail.com>
2020-09-22 15:01:03 -0500

    completed code with comments

---
+++ password_validation.py
@@ -0,0 +1,27 @@
+#import the regular expression module
+import re
+
+#the solution function validates the passweord passed to it
+def solution(S):
+    # this loop will run until False in invoke 
+    while True:
+        if (len(S)<6):#check that the lenght of S is more than 6
+            return False
+        elif not re.search(r"[a-z]", S):#use the regular expression search function to check for lowercase letters
+            return False
+        elif not re.search(r"[A-Z]", S):#use the regular expression search function to check for uppercase letters
+            return False
+        elif not re.search(r"[0-9]", S):#use the regular expression search function to check for digits
+            return False
+        elif not re.search(r"[!@#\$%^&\*()_]", S):#use the regular expression search function to check for special characters 
+            return False
+        elif re.search(r'\s', S):#use the regular expression search function to check for white spaces
+            return False
+        else:#if the previous checks pass the function will return True
+            return True
+
+#password 
+password = "mJGdp499&"
+
+#calling the function and printing the return value to the terminal
+print(solution(password))
